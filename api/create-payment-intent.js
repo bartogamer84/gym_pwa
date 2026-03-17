9c7a9312635b9c05ff683a2c61943abbafeb5ea6
bartogamer84 <emiliolicerio@gmail.com>
2026-03-17 08:00:52 -0600

    stripe_fix2

--- api/create-payment-intent.js
+++ api/create-payment-intent.js
@@ -32,7 +32,6 @@ export default async function handler(req, res) {
     const paymentIntent = await stripe.paymentIntents.create({
       amount, // en centavos
       currency,
-      payment_method_types: ['card'],
       description,
       automatic_payment_methods: { enabled: true },
     });
